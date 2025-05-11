62ded1811ca458b2411135ccb3b3e11092b65e7f
Jorge Manuel Ferreira Rosado <jormanue@c2r3s11.42porto.com>
2025-05-11 15:53:59 +0100

    refined test script to automatically build library and clean itself

--- tests/test.sh
+++ tests/test.sh
@@ -6,16 +6,23 @@
 #    By: jormanue <jormanue@student.42porto.co      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/05/11 15:29:27 by jormanue          #+#    #+#              #
-#    Updated: 2025/05/11 15:42:35 by jormanue         ###   ########.fr        #
+#    Updated: 2025/05/11 15:52:55 by jormanue         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+#build library
+echo "does your shit compile"
+make re
 #test counter
 T=0
 #build tests
 cc -Wall -Werror -Wextra -Iinclude tests/test.c -L. -lftprintf -o tests/test
 T=$((T+1))
 
-echo "built $T test"
+echo "$T tests are coming for you"
 
-echo "running tests"
+echo "and they run"
 ./tests/test 
+
+make fclean
+echo "i was never here"
