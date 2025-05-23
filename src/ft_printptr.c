2a034f967a6ba622c83326ea734d5252be3952d8
Ddifhens <jorge.rosadomfr99@gmail.com>
2025-05-23 17:27:09 +0100

    (nill) i guess

--- src/ft_printptr.c
+++ src/ft_printptr.c
@@ -3,10 +3,10 @@
 int	ft_printptr(void *ptr)
 {
 	int	p;
-	p = 0;
 
+	p = 0;
 	if (!ptr)
-		return (0)
+		return (ft_printstr("(nill)");
 	p += ft_printstr("0x");
 	return (p + (ft_puthex((unsigned long)ptr, 0)));
 }
